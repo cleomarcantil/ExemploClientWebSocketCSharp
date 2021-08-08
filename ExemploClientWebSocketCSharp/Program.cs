1afaf81c8699d48cdcfaed64ff14c7cd1128312a
Cleomar Cantil <cleomarcantil@outlook.com>
2021-08-08 00:45:43 -0300

    Arquivos iniciais do projeto.

---
+++ ExemploClientWebSocketCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExemploClientWebSocketCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Exemplo cliente de conexão a um WebSocket");
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
